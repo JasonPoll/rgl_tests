e481cbfb46b7657628c659ac99824b14ce168ee9
Jason Poll <jason.poll@ccisystems.com>
2018-08-15 12:01:13 -0500

    Add pry to gemfile, small refactor to bust_es

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'https://rubygems.org'
 
 ruby '2.4.4'
 gem 'activesupport', require: 'active_support/all'
+gem 'pry'
 gem 'rgl'
--- bust_es.rb
+++ bust_es.rb
@@ -3,12 +3,15 @@ require 'active_support/all'
 require 'rgl/adjacency'
 require 'rgl/dot'
 
-def fn
-  '/Users/jtpoll/Downloads/neat_wtf/neat02-edgeSuite-export-2018-08-07/entities/DataProducerEntityManager-AbstractTabularProducerDO.json'
+def talbe_nodes_filename=(val)
+  @table_nodes_filename = val
+end
+def table_nodes_filename
+  @table_nodes_filename ||= '/Users/jtpoll/Downloads/LAB_FULL_Edge343_20180710-181000/entities/DataProducerEntityManager-AbstractTabularProducerDO.json'
 end
 
-def data
-  @data ||= JSON.parse(File.read(fn)).deep_symbolize_keys
+def tabular_nodes_file_data
+  @data ||= JSON.parse(File.read(table_nodes_filename)).deep_symbolize_keys
 end
 
 # * Fugly massive PNG?  Gimp can view it.
@@ -26,7 +29,7 @@ end
 # weird_ones = tabular_transforms_raw.map{|h| [h[:properties][:propertyValues].find{|h2| h2[:propertyDefName] == 'name'}[:value], h[:properties][:propertyValues].find{|h2| h2[:propertyDefName] == 'attributes'}]}.select{|a| a.second.nil?}
 # => [["LM Device Group Map by Customer", nil], ["Open Tickets by Org", nil], ["ZenDesk Ticket Staus Count", nil], ["LM Nomadix Devices by Device Group ID", nil]]
 def tabular_transforms_raw
-  data[:objects].select{|h| h[:doClass] == 'TabularTransformDO'}
+  tabular_nodes_file_data[:objects].select{|h| h[:doClass] == 'TabularTransformDO'}
 end
 def tabular_transforms
   @tabulate_transforms ||= tabular_transforms_raw.map do |h|
@@ -41,19 +44,8 @@ def tabular_transforms
   end
 end
 
-def feed_name(h)
-   h[:properties][:propertyValues].find{|h2| h2[:propertyDefName] == 'name'}[:value]
-end
-
-def attribute_defs_raw(h)
-  (h[:properties][:propertyValues].find{|h2| h2[:propertyDefName] == 'attributes'} || {value: {selectedAttributeList: []}.to_json})[:value]
-end
-def attribute_defs(h)
-  JSON.parse(attribute_defs_raw(h)).deep_symbolize_keys[:selectedAttributeList].map{|h2| h2[:newAttributeName] || h2[:attributeName]}
-end
-
 def data_feeds_raw
-  data[:objects].select{|h| h[:doClass] == 'DataFeedDO'}
+  tabular_nodes_file_data[:objects].select{|h| h[:doClass] == 'DataFeedDO'}
 end
 def data_feeds
   @data_feeds ||= data_feeds_raw.map do |h|
@@ -77,6 +69,7 @@ def graphit
   graph
 end
 
+
 def grind_hash(h, graph)
   h.each do |child|
     input_feeds = data_feeds.select{|parent| child[:upstream_ids].include?(parent[:id])}
@@ -96,4 +89,16 @@ end
 
 def edge_text(h)
   "#{h[:name]}(#{h[:parameters].join(',')})\\n[#{h[:outputs].join(',')}]"
+end
+
+def feed_name(h)
+   h[:properties][:propertyValues].find{|h2| h2[:propertyDefName] == 'name'}[:value]
+end
+
+def attribute_defs_raw(h)
+  (h[:properties][:propertyValues].find{|h2| h2[:propertyDefName] == 'attributes'} || {value: {selectedAttributeList: []}.to_json})[:value]
+end
+
+def attribute_defs(h)
+  JSON.parse(attribute_defs_raw(h)).deep_symbolize_keys[:selectedAttributeList].map{|h2| h2[:newAttributeName] || h2[:attributeName]}
 end
